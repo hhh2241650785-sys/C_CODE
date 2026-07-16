3b9ad40b5495cca40a925f7d5cba61c3bf47e491
hhh2241650785 <hhh2241650785@gmail.com>
2026-07-16 23:19:22 +0800

    学了指针，和结构体的一些内容

---
+++ test_7_16/test_7_16/test.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	int* p = arr;
+//	for (i = 0; i < 10;i++)
+//	{
+//		*p++ = 1;
+//	}
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	while (*str != '\0')
+//	{
+//		str++;
+//	}
+//	return str - start;
+//}
+//
+//int main()
+//{
+//	int len = my_strlen("abcdef");
+//	printf("%d", len);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* parr[10] = { &a,&b,&c };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *parr[i]);
+//	}
+//	return 0;
+//}  
