88031992f36f51f41818bd979ff6292e9e1f6d81
hhh2241650785 <hhh2241650785@gmail.com>
2026-07-19 21:20:05 +0800

    今天主要是跟着视频写了扫雷的基础代码，还有写了其他一点点的题目代码

---
+++ test_7_19/test_7_19/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int tmp = 1;
+//	int x = a;
+//	int y = b;
+//	while (tmp)
+//	{
+//		tmp = a % b;
+//		a = b;
+//		b = tmp;
+//	}
+//	printf("%d", x*y/a);
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	char arr[101] = { 0 };
+	while (scanf("%s", &arr + i))
+	{
+		;
+	}
+	int right = &arr + i;
+	int left = arr;
+	
+	while (left < right)
+	{
+
+	}
+	return 0;
+}
